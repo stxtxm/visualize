5f0f06cf4fefb22c0715cef90c0a8bf6590f8cb4
Timo <sitizstorm@gmail.com>
2026-07-23 00:24:42 +0200

    Bump version to 0.2.1

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 # Si on est en mode dev, on essaie de récupérer dynamiquement la version via git
 if __version__ == "dev":
